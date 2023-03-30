1a87f395bc3b3bd78e8ac6f3ce46255a9e6d4d51
FERREIRAR <romain.cdesafer@gmail.com>
2023-03-30 15:26:54 +0200

    30/03/2023

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="stylesIndex.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+</head>
+<body>
+<header>
+    <!--    -----------------------------
+                La navbar
+    -----------------------------  -->
+    <nav  class=" navbar navbar-dark shadow mb-5 sticky-top bg-gradent" style="background: linear-gradient(#0b152c,#0f1c3c)">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">FERREIRA Romain</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarDark" aria-controls="offcanvasNavbarDark">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarDark" aria-labelledby="offcanvasNavbarDarkLabel" style="background: linear-gradient(#0f1c3c,#0b152c)">
+                <div class="offcanvas-header d-flex justify-content-end">
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body" >
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <a class="ep-btn nav-link active d-flex justify-content-center my-2"  href=""><h4>Home</h4></a>
+                        <a class="ep-btn nav-link active d-flex justify-content-center my-2"  href=""><h4>Formations</h4></a>
+                        <a class="ep-btn nav-link active d-flex justify-content-center my-2"  href=""><h4>Compétences</h4></a>
+                        <a class="ep-btn nav-link active d-flex justify-content-center my-2"  href=""><h4>Expériences Professionnelles</h4></a>
+                        <a class="ep-btn nav-link active d-flex justify-content-center my-2"  href=""><h4>Projets</h4></a>
+                        <a class="ep-btn nav-link active d-flex justify-content-center my-2"  href=""><h4>Veilles</h4></a>
+                        <a class="ep-btn nav-link active d-flex justify-content-center my-2"  href=""><h4>Loisirs</h4></a>
+                        <a class="ep-btn nav-link active d-flex justify-content-center my-2"  href=""><h4>Contacts</h4></a>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</header>
+<div>
+
+    <p style="background-color: #FFB116">.</p>
+    <p style="background-color: #FC9100">.</p>
+    <p style="background-color: #003051">.</p>
+    <p style="background-color: #004792">.</p>
+    <p style="background-color: #3f7caf">.</p>
+    <p style="background-color: #7a672e">.</p>
+    <p style="background-color: #0f1c3c">.</p>
+    <p style="background-color: #244269">.</p>
+    <p style="background-color: #e8edf9">.</p>
+
+</div>
+<section id="Qui suis-je ?">
+<div>
+    <h2>Qui suis-je ?</h2>
+    <p></p>
+</div>
+</section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
